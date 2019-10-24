0cf7f76cfd0a5a7f832e7d65d5274ae17c057407
Ben <864129714@qq.com>
2019-10-24 21:38:09 +0800

    删除GitHub网站实验

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1571920946740}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1571924256533}
